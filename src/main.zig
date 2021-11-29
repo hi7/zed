2a1e4b80f59d39e8e363ea7c7f6d3330a64db02d
Hilger Baumstark <hilger.zone@gmail.com>
2021-11-29 20:15:12 +0100

    move action shortcut to menu

--- src/main.zig
+++ src/main.zig
@@ -12,7 +12,7 @@ var cursor_x: usize = 1;
 var cursor_y: usize = 2;
 var filename: []u8 = "";
 var textbuffer: []u8 = "";
-const keyCodeOffset = 33;
+const keyCodeOffset = 20;
 
 pub fn main() anyerror!void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -81,6 +81,9 @@ fn setMenuBarMode(allocator: Allocator) void {
     term.resetMode();
     term.setAttributeMode(Mode.underscore, Scope.foreground, themeColor, allocator);
 }
+fn setMenuBarHighlightMode(allocator: Allocator) void {
+    term.setAttributeMode(Mode.reset, Scope.light_foreground, themeColor, allocator);
+}
 fn setStatusBarMode(allocator: Allocator) void {
     term.resetMode();
     term.setAttributeMode(Mode.reverse, Scope.foreground, themeColor, allocator);
@@ -98,32 +101,49 @@ fn showMessage(message: []const u8, allocator: Allocator) void {
     term.write(message);
     term.resetMode();
 }
-var first_line: u16 = 1;
-fn writeScreen(allocator: Allocator) void {
+
+fn shortCut(key: u8, name: []const u8, allocator: Allocator) void {
+    setMenuBarHighlightMode(allocator);
+    term.writeByte(key);
+    setMenuBarMode(allocator);
+    term.write(name);
+}
+inline fn menuBar(allocator: Allocator) void {
     term.clearScreen();
     setMenuBarMode(allocator);
     term.cursorHome();
     repearChar(' ', width);
 
+    term.setCursor(width - 12, 1, allocator);
+    shortCut('Q', "uit: Ctrl-q", allocator);
+}
+fn fileColor(modified: bool) Color {
+    return if(modified) Color.yellow else Color.white;
+}
+inline fn statusBar(allocator: Allocator) void {
     setStatusBarMode(allocator);
     term.setCursor(0, height, allocator);
     const offset = width - keyCodeOffset;
     repearChar(' ', offset);
 
-    term.setCursor(0, height, allocator);
-    term.setAttributesMode(Mode.reverse, Scope.light_foreground, themeColor, Scope.background, Color.yellow, allocator);
-    term.write(filename);
-
     setStatusBarMode(allocator);
     term.setCursor(offset, height, allocator);
-    term.write("key code:             ");
-    term.write("exit: Ctrl-q");
+    term.write("key code:            ");
 
-
+    term.setCursor(0, height, allocator);
+    term.setAttributesMode(Mode.reverse, Scope.foreground, themeColor, Scope.background, fileColor(false), allocator);
+    term.write(filename);
+}
+inline fn showTextBuffer(allocator: Allocator) void {
     term.resetMode();
-    term.setCursor(1, first_line + 1, allocator);
+    term.setCursor(1, 2, allocator);
     term.write(textbuffer);
     term.setCursor(cursor_x, cursor_y, allocator);
+}
+fn writeScreen(allocator: Allocator) void {
+    menuBar(allocator);
+    statusBar(allocator);
+    showTextBuffer(allocator);
 }
 
 fn writeChar(char: u8, allocator: Allocator) void {
@@ -140,12 +160,12 @@ fn writeKeyCodes(sequence: [4]u8, len: usize, posx: usize, posy: usize, allocato
     setStatusBarMode(allocator);
     term.setCursor(posx, posy, allocator);
     term.write("           ");
-    term.setAttributesMode(Mode.reverse, Scope.light_foreground, themeColor, Scope.background, Color.white, allocator);
+    term.setAttributesMode(Mode.reverse, Scope.foreground, themeColor, Scope.background, Color.white, allocator);
     term.setCursor(posx, posy, allocator);
     if(len == 0) return;
     if(len == 1) print("{x}", .{sequence[0]});
-    if(len == 2) print("{x}\x1b[1C{x}", .{sequence[0], sequence[1]});
-    if(len == 3) print("{x}\x1b[1C{x}\x1b[1C{x}", .{sequence[0], sequence[1], sequence[2]});
-    if(len == 4) print("{x}\x1b[1C{x}\x1b[1C{x}\x1b[1C{x}", .{sequence[0], sequence[1], sequence[2], sequence[3]});
+    if(len == 2) print("{x} {x}", .{sequence[0], sequence[1]});
+    if(len == 3) print("{x} {x} {x}", .{sequence[0], sequence[1], sequence[2]});
+    if(len == 4) print("{x} {x} {x} {x}", .{sequence[0], sequence[1], sequence[2], sequence[3]});
     term.resetMode();
 }
